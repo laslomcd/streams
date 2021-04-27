fab243bfb967b67b68a3a584d47f4a1a2ec38cda
ryan <laslomcd@gmail.com>
2021-04-27 17:06:23 -0400

    setup project and install router

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+
+const PageOne = () => {
+	return <div>Page One</div>;
+};
+
+const PageTwo = () => {
+	return (
+		<div>
+			Page Two
+			<button>Click Me</button>
+		</div>
+	);
+};
+
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" exact component={PageOne} />
+					<Route path="/pagetwo" component={PageTwo} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
