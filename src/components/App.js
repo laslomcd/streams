a96810e0a5eff871ae1dbf689b199df2b420a21c
ryan <laslomcd@gmail.com>
2021-05-06 17:05:26 -0400

    started router in new project

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,24 @@
 import React from "react";
-import { Route, BrowserRouter, Link } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 
-const PageOne = () => {
-	return (
-		<div>
-			Page One
-			<Link to="/pagetwo">Page Two</Link>
-		</div>
-	);
-};
-
-const PageTwo = () => {
-	return (
-		<div>
-			Page Two
-			<Link to="/">Page One</Link>
-		</div>
-	);
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
 	return (
-		<div>
+		<div className="ui container">
+			<Header />
 			<BrowserRouter>
 				<div>
-					<Route path="/" exact component={PageOne} />
-					<Route path="/pagetwo" component={PageTwo} />
+					<Route path="/" exact component={StreamList} />
+					<Route path="/streams/new" component={StreamCreate} />
+					<Route path="/streams/edit" exact component={StreamEdit} />
+					<Route path="/streams/delete" component={StreamDelete} />
+					<Route path="/streams/show" component={StreamShow} />
 				</div>
 			</BrowserRouter>
 		</div>
