bcc6e815da40e8f26a038288ee86bfaf18f1a139
Ryan McDonagh <laslomcd@gmail.com>
2021-10-13 17:15:13 -0400

    added stream reducer and rendered list of streams

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,8 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const CREATE_STREAM = 'CREATE_STEAM';
+export const FETCH_STREAMS = 'FETCH_STEAMS';
+export const FETCH_STREAM = 'FETCH_STEAM';
+export const DELETE_STREAM = 'DELETE_STREAM';
+export const EDIT_STREAM = 'EDIT_STREAM';
+
